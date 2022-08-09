a34ddd7fc0a5ccff6b929759f0be222a52da25e5
atifadamu <110117412+atifadamu@users.noreply.github.com>
2022-08-09 17:59:34 +0000

    lesson2 first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+if (3 == 3 && 4 == 4) {
+  document.write("the condition is true");
+} else {
+  document.write("the condition is false and it will not be printed");
+}
+const age = 20;
+if (age > 18) {
+  alert("old enough to vote");
+} else {
+  alert("Not old enough to register");
+}
+const x = 20;
+if (x < 10) {
+  document.getElementById("demo2").innerHTML = "X is less than 10";
+} else if (x > 10) {
+  document.getElementById("demo2").innerHTML = "X is greater than10";
+} else {
+  document.getElementById("demo2").innerHTML = "X is egual 10";
+}
